ce06023ad9e1cb77228bffcb5b475399e8f45efc
GMcD <gary.macdonald@projectscapa.com>
2013-12-08 15:27:26 +0000

    Simple Initial layout for Yeoman Generator template.

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ Scaffold
 ========
 
 Mobile App Scaffolding
+
+#Configuration - Without Yeoman
+
+git clone Scaffold
+
+Edit App id etc
+
+*bower install
+*npm install
+*grunt setup
+*grunt 
+
---
+++ app/tests/home.js
@@ -0,0 +1,13 @@
+define(['home'], function(home) {
+
+	return describe('Backbone Home :: ', function() {
+
+		describe('View :: ', function() {
+			it('is Welcome Home!', function(){
+				var view = Home.HomeView();
+				var content = $('div#stage p').text();
+				expect(content).toEqual('Welcome Home!');
+			});
+		});		
+	});
+});
